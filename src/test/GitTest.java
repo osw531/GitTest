ac689a2ac7956e5d00907a5b62dbb05283996610
ohseone <ohseone0520@gmail.com>
2019-01-30 13:05:01 +0900

    ver 1.01

--- src/test/GitTest.java
+++ src/test/GitTest.java
@@ -2,7 +2,7 @@ package test;
 
 public class GitTest {
 	public void test() {
-		
+		System.out.println("Å×½ºÆ®");
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
